e02297d0ebc0408e38b011c8bde846b6d45e001b
Jkuzz <honza.ku@gmail.com>
2022-09-13 09:38:42 +0200

    Added exec script

---
+++ exec.sh
@@ -0,0 +1,8 @@
+module_path="./cz.cuni.mff.java.projects.posapp.core/target/jars/;\
+./cz.cuni.mff.java.projects.posapp.database/target/jars/;\
+./cz.cuni.mff.java.projects.posapp.tables/target/jars/;\
+./cz.cuni.mff.java.projects.posapp.inventory/target/jars/;\
+./cz.cuni.mff.java.projects.posapp.products/target/jars/;\
+./cz.cuni.mff.java.projects.posapp.payment/target/jars/;"
+
+java -p $module_path --module cz.cuni.mff.java.projects.posapp.core/cz.cuni.mff.java.projects.posapp.core.App
